689ceb166e301adf7996cb9c2b278e241e098bb5
Christopher Woggon <christopher.woggon@gmail.com>
2025-09-01 12:16:07 +0900

    filtering out duplicates

--- sort_and_filter_spamdomains.py
+++ sort_and_filter_spamdomains.py
@@ -1,7 +1,7 @@
 with open("spamdomains.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
-lines = sorted(line.strip() for line in lines if line.strip())
+lines = sorted(list(set(line.strip() for line in lines if line.strip())))
 
 with open("spamdomains.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(lines) + "\n")
